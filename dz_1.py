bac329fcaf2796df2d5ab79b02a90268e1366b10
ReZoNyan <maksim20000109@gmail.com>
2024-11-05 23:45:31 +0500

    Add files via upload

---
+++ dz_1.py
@@ -0,0 +1,12 @@
+from fake_math import divde as fls
+from true_math import divde as tru
+
+res_1 = fls(69, 3)
+res_2 = fls(3, 0)
+res_3 = tru(49, 7)
+res_4 = tru(15, 0)
+
+print(res_1)
+print(res_2)
+print(res_3)
+print(res_4)
